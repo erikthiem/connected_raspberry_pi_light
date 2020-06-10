64bdc04e60701f5426865d9e748e58272e76c706
Erik Thiem <erikthiem@gmail.com>
2020-06-10 13:58:33 -0400

    Remove unncessary camera route

--- lib/network_led/http.ex
+++ lib/network_led/http.ex
@@ -59,22 +59,6 @@ defmodule NetworkLed.Http do
     |> send_resp(200, markup)
   end
 
-  get "/camera" do
-    markup = """
-    <html>
-    <head>
-      <title>Picam Video Stream</title>
-    </head>
-    <body>
-      <img src="video.mjpg" />
-    </body>
-    </html>
-    """
-    conn
-    |> put_resp_header("Content-Type", "text/html")
-    |> send_resp(200, markup)
-  end
-
   forward "/video.mjpg", to: NetworkLed.Streamer
 
   match(_, do: send_resp(conn, 404, "Oops!"))
