81f9ac5e4849b6991453131c1fc9acbec768dea0
Erik Thiem <erikthiem@gmail.com>
2020-06-10 13:55:41 -0400

    Add ajax buttons for a better UI

--- lib/network_led/http.ex
+++ lib/network_led/http.ex
@@ -22,11 +22,33 @@ defmodule NetworkLed.Http do
     </head>
     <body>
       <h1>Control the light</h1>
-      <a href="/enable">Enable</a>
-      <a href="/disable">Disable</a>
+      <button id="enable">On</button>
+      <button id="disable">Off</button>
+      <br/>
       <img src="video.mjpg" />
+
+      <script
+        src="https://code.jquery.com/jquery-3.5.1.min.js"
+        integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+        crossorigin="anonymous"></script>
+
       <script type="text/javascript">
-        window.alert("testing");
+        $(document).ready(function() {
+
+          $("#enable").click(function() {
+            $.ajax({url: "/enable", success: function(result){
+              console.log("turning light on");
+            }});
+          });
+
+          $("#disable").click(function() {
+            $.ajax({url: "/disable", success: function(result){
+              console.log("turning light off");
+            }});
+          });
+
+        });
+
 
       </script>
     </body>
